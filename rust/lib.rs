4564324f92b27c0e67ebc392355c4e81c46b366a
Christopher Gilliard <christopher.gilliard@gmail.com>
2025-05-06 18:19:16 -0700

    add impl

---
+++ rust/lib.rs
@@ -0,0 +1,21 @@
+#![no_std]
+
+extern crate result;
+
+use result::Result;
+
+pub trait TryClone {
+    fn try_clone(&self) -> Result<Self>
+    where
+        Self: Sized;
+}
+
+impl<T: Clone> TryClone for T {
+    fn try_clone(&self) -> Result<Self> {
+        Ok(self.clone())
+    }
+}
+
+pub fn real_main(_argc: i32, _argv: *const *const i8) -> i32 {
+    0
+}
