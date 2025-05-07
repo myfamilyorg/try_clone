8ede3cc8f2c301dcf4d67f40769fdd408c869ff1
Christopher Gilliard <christopher.gilliard@gmail.com>
2025-05-07 13:08:10 -0700

    use macro in test

--- rust/lib.rs
+++ rust/lib.rs
@@ -1,6 +1,13 @@
 #![no_std]
 
+extern crate macros;
 extern crate result;
+use macros::Dummy;
+
+#[derive(Dummy)]
+struct MyStruct {
+    x: i32,
+}
 
 use result::Result;
 
@@ -16,6 +23,12 @@ impl<T: Clone> TryClone for T {
     }
 }
 
-pub fn real_main(_argc: i32, _argv: *const *const i8) -> i32 {
-    1
+pub fn real_main(argc: i32, _argv: *const *const i8) -> i32 {
+    let x = MyStruct { x: 3 };
+    let y = MyStruct { x: argc };
+    if x == y {
+        x.x
+    } else {
+        0
+    }
 }
