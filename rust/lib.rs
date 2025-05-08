72e0fcdf65d652f1bf1e3b5c7ab684e5422ec7f4
Christopher Gilliard <christopher.gilliard@gmail.com>
2025-05-07 21:31:09 -0700

    clean up

--- rust/lib.rs
+++ rust/lib.rs
@@ -1,14 +1,6 @@
 #![no_std]
 
-extern crate macros;
 extern crate result;
-use macros::Dummy;
-
-#[derive(Dummy)]
-struct MyStruct {
-    x: i32,
-}
-
 use result::Result;
 
 pub trait TryClone {
@@ -20,15 +12,5 @@ pub trait TryClone {
 impl<T: Clone> TryClone for T {
     fn try_clone(&self) -> Result<Self> {
         Ok(self.clone())
-    }
-}
-
-pub fn real_main(argc: i32, _argv: *const *const i8) -> i32 {
-    let x = MyStruct { x: 3 };
-    let y = MyStruct { x: argc };
-    if x == y {
-        x.x
-    } else {
-        0
     }
 }
